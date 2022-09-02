8cd3ec7e1c22d005db61631d9bed9db31e33ccb0
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-09-02 15:44:26 +0300

    Redux ToolKit Practice

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postSlice from "../features/post/postSlice";
 import todoSlice from "../features/todo/todoSlice";
 import userSlice from "../features/user/userSlice";
 
@@ -6,6 +7,7 @@ const store = configureStore({
     reducer: {
         user: userSlice,
         todo: todoSlice,
+        post: postSlice,
     }
 })
 
